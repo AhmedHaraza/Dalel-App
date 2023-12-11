795ff5fbdc1a4cbc5752f711daa0f48e5959f760
Ahmed Haraza <ahmed.haraza755@gmail.com>
2023-12-11 11:52:40 +0200

    Structure project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Dalel());
+}
+
+class Dalel extends StatelessWidget {
+const Dalel({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
